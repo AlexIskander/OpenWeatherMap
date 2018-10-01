6a06abf0cac87b462c8068a577568033b381eddc
Alex <1111-1111@ukr.net>
2018-10-01 15:23:53 +0300

    modified Jenkinsfile

--- prepare-infra.sh
+++ prepare-infra.sh
@@ -6,4 +6,4 @@ cd $WORKSPACE
 
 /home/alex/softserve/docker_work/terraform init
 /home/alex/softserve/docker_work/terraform plan --out myplan
-home/alex/softserve/docker_work/terraform aplly 'myplan'
+/home/alex/softserve/docker_work/terraform aplly 'myplan'
