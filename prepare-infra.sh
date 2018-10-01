797ca14106d8eaa7ac6d5928633ab453fc7fdb7a
Alex <1111-1111@ukr.net>
2018-10-01 15:48:28 +0300

    modified Jenkinsfile

--- prepare-infra.sh
+++ prepare-infra.sh
@@ -3,7 +3,8 @@
 set -e
 
 cd $WORKSPACE/terraform
-
+/home/alex/softserve/docker_work/terraform refresh
 /home/alex/softserve/docker_work/terraform init
 /home/alex/softserve/docker_work/terraform plan --out myplan
-/home/alex/softserve/docker_work/terraform aplly 'myplan'
+/home/alex/softserve/docker_work/terraform apply "myplan"
+/home/alex/softserve/docker_work/terraform show
