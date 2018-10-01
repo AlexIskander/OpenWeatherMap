ac905a38e517fee2ed6d74bc90dffb2926047539
Alex <1111-1111@ukr.net>
2018-10-01 13:18:05 +0300

    added terraform file main.tf

--- Jenkinsfile
+++ Jenkinsfile
@@ -6,4 +6,13 @@ node {
         sh './manage.py test openweathermap'
     }
 
+    stage('TF Plan') {
+       steps {
+         container('terraform') {
+           sh 'terraform init'
+           sh 'terraform plan -out myplan'
+         }
+       }
+     }
+
 }
---
+++ terraform/main.tf
@@ -0,0 +1,57 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_instance" "example" {
+  ami = "ami-0f5dbc86dd9cbf7a8"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+  key_name = "${aws_key_pair.my_key.key_name}"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum install git -y
+              yum install python-pip -y
+              mkdir /var/www/
+              git clone https://github.com/AlexIskander/OpenWeatherMap
+              cd OpenWeatherMap
+              pip install -r requirements.txt
+              python manage.py migrate   
+              nohup python manage.py runserver 0.0.0.0:"${var.server_port}" &
+              EOF
+
+  tags {
+    name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+}
+}
+
+resource "aws_key_pair" "my_key" {
+  key_name   = "my_key"
+  public_key = "${file("~/.ssh/id_rsa.pub")}"
+}
+
+output "public_ip" {
+  value = "${aws_instance.example.public_ip}"
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  default = 8888
+}
